cdb59cd93c0da887655e42299c6e833bcd6be2f4
cedney97 <c.edney@wustl.edu>
2023-12-18 22:21:47 -0600

    init commit

---
+++ src/components/FightScreen.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import styles from '../styles/FightScreen.module.css'
+
+const FightScreen: FC = () => {
+    return <div className={styles.fight_screen}></div>
+}
+
+export default FightScreen
