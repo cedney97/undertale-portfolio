27c8f1496701e206761f812df7f1e2e32d258990
cedney97 <c.edney@wustl.edu>
2023-12-21 13:29:30 -0600

    Trying to get multiple typewriters to work

--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -1,20 +1,29 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
 import { PlayFunction } from 'use-sound/dist/types'
 import styles from '../styles/FightScreen.module.css'
 
 interface Props {
     text: string,
     delay: number,
-    playSound?: PlayFunction
+    playSound?: PlayFunction,
+    readyToType?: boolean,
+    setReadyToType?: Dispatch<SetStateAction<boolean>>
 }
 
-const Typewriter: FC<Props> = ({ text, delay, playSound }) => {
+const Typewriter: FC<Props> = ({
+    text,
+    delay,
+    playSound,
+    readyToType,
+    setReadyToType
+}) => {
     const [audioDelay, setAudioDelay] = useState<boolean>(true)
     const [currentText, setCurrentText] = useState<string>("")
     const [currentIndex, setCurrentIndex] = useState<number>(0)
 
+
     useEffect(() => {
-        if (currentIndex < text.length) {
+        if (currentIndex < text.length && readyToType) {
             const timeout = setTimeout(() => {
                 if (playSound && audioDelay) {
                     setAudioDelay(false)
@@ -27,14 +36,18 @@ const Typewriter: FC<Props> = ({ text, delay, playSound }) => {
             }, delay);
 
             return () => clearTimeout(timeout);
+        } else {
+            if (setReadyToType) {
+                setReadyToType(true)
+            }
         }
-    }, [currentIndex, delay, text]);
+    }, [currentIndex, delay, text, readyToType]);
 
-    return <div>
+    return <>
         {currentText.split("\\n").map((line, index) => {
             return <div key={index}>{line}</div>
         })}
-    </div>
+    </>
 }
 
 export default Typewriter
--- src/components/consoleDisplays/ContactConsole.tsx
+++ src/components/consoleDisplays/ContactConsole.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from '../styles/FightScreen.module.css'
+import React, { useEffect, useState } from 'react'
+import styles from '../../styles/FightScreen.module.css'
 
 import Typewriter from '../Typewriter'
 import useSound from 'use-sound'
@@ -9,10 +9,65 @@ const ContactConsole = () => {
 
     const [playTextSound] = useSound(textSound)
 
+    const [emailType, setEmailType] = useState<boolean>(false)
+    const [checkType, setCheckType] = useState<boolean>(false)
+    const [gitHubType, setGitHubType] = useState<boolean>(false)
+    const [moreType, setMoreType] = useState<boolean>(false)
+
+    useEffect(() => {
+        console.log("ready to type check", checkType)
+    }, [checkType])
+
     return (
-        <>
-            *&nbsp;<Typewriter text="Contact Console" delay={50} playSound={playTextSound} />
-        </>
+        <div className={styles.console_content} style={{ flexDirection: "row" }}>
+            *&nbsp;
+            <Typewriter
+                text="Feel free to email me at"
+                delay={25}
+                playSound={playTextSound}
+                readyToType={true}
+                setReadyToType={setEmailType}
+            />
+            &nbsp;
+            <a href="mailto:cadeedney@gmail.com" className={styles.chosen}>
+                <Typewriter
+                    text="cadeedney@gmail.com"
+                    delay={25}
+                    playSound={playTextSound}
+                    readyToType={emailType}
+                    setReadyToType={setCheckType}
+                />
+            </a>
+            &nbsp;
+            <span>
+                <Typewriter
+                    text="or check out my"
+                    delay={25}
+                    playSound={playTextSound}
+                    readyToType={checkType}
+                    setReadyToType={setGitHubType}
+                />
+            </span>
+            &nbsp;
+            <a href="https://github.com/cedney97">
+                <Typewriter
+                    text="GitHub"
+                    delay={25}
+                    playSound={playTextSound}
+                    readyToType={gitHubType}
+                    setReadyToType={setMoreType}
+                />
+            </a>
+            & nbsp;
+            <span>
+                <Typewriter
+                    text="for more of my work!"
+                    delay={25}
+                    playSound={playTextSound}
+                    readyToType={moreType}
+                />
+            </span>
+        </div >
     )
 }
 
